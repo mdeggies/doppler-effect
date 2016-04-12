2e8939addbc494997d6dc8e1c8859564ec7915fd
Michele Degges <mdeggies@gmail.com>
2016-04-12 16:09:43 -0700

    first draft

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Doppler Effect
+
+1. The star’s (1) colour should change based on the value of the velocity input (2), where:
+    a. 0 = no colour change
+    b. ‐100 = Blue
+    c. 100 = Red
+2. The slider (3) should update the velocity input (2), in the range ‐100 to 100 inclusive.
+3. The velocity input should accept numbers only; which are limited to range of the slider (3).
+4. The slider should be based on either a log or exponential scale, providing more detail towards the negative velocity range (Blue).
+
+## Resources:
+
+http://ionden.com/a/plugins/ion.rangeSlider/en.html
+http://jsfiddle.net/pandey_mohit/BeSwL/
+http://jsfiddle.net/IonDen/zqh4o7uc/
+
+https://codepen.io/juliangarnier/pen/idhuG
---
+++ main.js
@@ -0,0 +1,128 @@
+$(document).ready(init);
+
+function init() {
+  var hexMap = hexMapper();
+
+  TweenMax.to(".star", 4, {y:150, x:400, ease:Linear.easeIn, scaleX: 0.1, scaleY: 0.1, repeat: -1});
+  //TweenMax.to(".star", 4, {y:150, x:0, ease:Linear.easeOut, scaleX: 0.1, scaleY: 0.1, repeat: -1})
+
+  // TweenLite.to(".star", 4, {rotationY:100, ease:Linear.easeIn, scaleX:0.1, scaleY:.1, repeat:10, repeatDelay: 5,
+  //   onComplete:function() {
+  //     TweenLite.to(".star", 4, {rotationX:180, ease:Linear.easeOut, scaleX:0.8, scaleY:.8, repeat:10, repeatDelay: 5});
+  //   }
+  // });
+
+  var $range = $(".js-range-slider"),
+  $from = $(".js-from"),
+  range,
+  min = -100,
+  max = 100
+
+  var updateValues = function () {
+    //bind slider and input box values
+    $from.prop("value", from);
+    var num = from;
+    num = Math.ceil(num/2);
+    var hexValue = hexMap[num];
+    //change the color of the star based on input values
+    colorImage('starImg', hexValue);
+  };
+
+  $range.ionRangeSlider({
+    type: "single",
+    min: min,
+    max: max,
+    prettify_enabled: true,
+    grid: true,
+    grid_num: 5,
+    onChange: function (data) {
+      from = data.from;
+      updateValues();
+    }
+  });
+
+  range = $range.data("ionRangeSlider");
+
+  var updateRange = function () {
+    range.update({
+      from: from
+    });
+  };
+
+  $from.on("change", function () {
+    from = +$(this).prop("value");
+    if (from < min) {
+      from = min;
+    }
+    updateValues();
+    updateRange();
+  });
+}
+
+function hexMapper() {
+  var hexMap = {};
+
+  var whiteToBlue = jsgradient.generateGradient('#FFFFFF', '#0000FF', 51); //0 to -50
+  var whiteToRed = jsgradient.generateGradient('#FFFFFF', '#FF0000', 51); //0 to 50
+
+  for (var i=whiteToBlue.length-1; i>=1; i--) {
+    var value = whiteToBlue[i];
+    var key = '-'+i;
+    hexMap[key] = value;
+  }
+  for (var i=0; i<whiteToRed.length; i++) {
+    var value = whiteToRed[i];
+    var key = i.toString();
+    hexMap[key] = value;
+  }
+
+  return hexMap;
+}
+
+function hexToRgb(color) {
+  //convert hex to rgb values
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  color = color.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : {
+    r: 0,
+    g: 0,
+    b: 0
+  };
+}
+
+function colorImage(imgId,hexaColor) {
+  //create hidden canvas (using image dimensions)
+  var imgElement = document.getElementById(imgId);
+
+  var canvas = document.createElement("canvas");
+  canvas.width = imgElement.width;
+  canvas.height = imgElement.height;
+
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(imgElement,0,0);
+
+  var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+
+  var data = imageData.data;
+
+  //convert image to grayscale
+  var rgbColor = hexToRgb(hexaColor);
+
+  for (var p = 0, len = data.length; p < len; p+=4) {
+    data[p + 0] = rgbColor.r;
+    data[p + 1] = rgbColor.g;
+    data[p + 2] = rgbColor.b;
+  }
+  ctx.putImageData(imageData, 0, 0);
+
+  //replace image source with canvas data
+  imgElement.src = canvas.toDataURL();
+}
